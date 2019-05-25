91980bcc41cce20202075a97959ade0c315cbbc6
Fabio Romanini <fabioaromanini@gmail.com>
2019-05-25 13:29:28 -0300

    email-endpoint - adds sendgrid api

--- handler.js
+++ handler.js
@@ -1,19 +1,29 @@
+const sgMail = require('@sendgrid/mail');
+
 /* Required for CORS */
 const headers = {
   'Access-Control-Allow-Origin': '*',
   'Access-Control-Allow-Credentials': true
 };
 
-const SENDGRID_API_KEY = process.env.SENDGRID_API_KEY;
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+const msg = {
+  to: 'marmelada@mailinator.com',
+  from: 'test@example.com',
+  subject: 'Sending with Twilio SendGrid is Fun',
+  text: 'and easy to do anywhere, even with Node.js',
+  html: '<strong>and easy to do anywhere, even with Node.js</strong>'
+};
 
 module.exports.email = (event, context, callback) => {
+  sgMail.send(msg);
+
   callback(null, {
     headers,
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        SENDGRID_API_KEY
+        message: 'Go Serverless v1.0! Your function executed successfully!'
       },
       null,
       2
